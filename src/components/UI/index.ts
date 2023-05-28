dc6974eab8f61376845a9b312f0520a2a7979476
aleksandr-onopriyenko <onopriyenkoalex@gmail.com>
2023-05-28 19:29:21 +0300

    refactor: add exports from components/UI

---
+++ src/components/UI/index.ts
@@ -0,0 +1,6 @@
+export * from "./Container/Container";
+export * from "./ErrorPage/ErrorPage";
+export * from "./Header/Header";
+export * from "./Input/Input";
+export * from "./Loader/Loader";
+export * from "./RegionSelect/RegionSelect";
