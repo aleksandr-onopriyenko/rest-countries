a66423378ca47662bd9a9215b3a39863e3deef1b
aleksandr-onopriyenko <onopriyenkoalex@gmail.com>
2023-05-28 19:37:36 +0300

    refactor: add exports from hooks

---
+++ src/hooks/index.ts
@@ -0,0 +1,6 @@
+export { useDebounce } from "./useDebounce";
+export { useCountriesAll } from "./useCountriesAll";
+export { useCountriesData } from "./useCountriesData";
+export { useCountryBorders } from "./useCountryBorders";
+export { useCountriesBySearch } from "./useCountriesBySearch";
+export { useCountriesByRegion } from "./useCountriesByRegion";
